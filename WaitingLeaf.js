9180dfc9de6d86a341acd2b4ac73487ba9490e0c
haochunyang <lanianer@qq.com>
2019-08-30 09:31:55 +0800

    在waiting页面添加了一个图片展示

--- WaitingLeaf.js
+++ WaitingLeaf.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {
-    StyleSheet, Text, View
+    StyleSheet, Text, View, Image,
 } from 'react-native';
 import { extend } from 'dayjs';
 
@@ -10,8 +10,20 @@ export default class WaitingLeaf extends React.PureComponent {
         super(proos);
     }
     render() {
+
+        let imageSource = {
+            // uri : 'https://img2018.cnblogs.com/blog/1040075/201901/1040075-20190116111956008-530354835.png',
+            uri : 'https://img2018.cnblogs.com/blog/1040075/201901/1040075-20190116111626566-10951833.png',
+            // headers : {
+            //     Authorization1 : 'someAuthToken',
+            //     Authorization2 : 'someAuthToken',
+            // }
+        }
+
         return (
             <View style = {styles.container}>
+                <Image style = {styles.imageStyle} 
+                    source = {imageSource}/>
                 <Text style = {styles.textPromptStyle}>
                     登录使用手机号码：{this.props.phoneNumber}
                 </Text>
@@ -41,6 +53,10 @@ let styles = StyleSheet.create ({
         justifyContent : 'center',
         alignItems : 'center',
         backgroundColor : '#F5FCFF',
+    },
+    imageStyle : {
+        width : 300,
+        height : 300,
     },
     textPromptStyle : {
         fontSize : 20,
